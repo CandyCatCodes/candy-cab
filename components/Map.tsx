e3a9b7c0ecd34c7896b7df8931505608dad0f886
CandyCatCodes <candydavies22@gmail.com>
2025-03-10 01:13:44 +0000

    add native and web maps and fix PWA scrolling

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,37 @@
-import { Text, View } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "@/components/MapView";
+import React from "react";
+import { Platform, StyleSheet, Text } from "react-native";
+// import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 const Map = () => {
+  // const region = {}
   return (
-    <View>
+    <MapView
+      provider={Platform.OS === "web" ? "google" : PROVIDER_DEFAULT}
+      googleMapsApiKey={`${process.env.EXPO_PUBLIC_GOOGLE_API_KEY}`}
+      className="flex-1 w-full h-full rounded-2xl"
+      tintColor="black"
+      style={styles.map}
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      //initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
       <Text>Map</Text>
-    </View>
+    </MapView>
   );
 };
+
+// TODO: Implement using a remap as per https://www.nativewind.dev/guides/third-party-components
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    borderRadius: 16,
+    tintColor: "black",
+  },
+});
 
 export default Map;
